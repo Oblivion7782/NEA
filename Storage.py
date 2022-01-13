f57b69875b6de9b7d0ef0ac2d1dadaa01d3ae413
Oblivion7782 <71395312+Oblivion7782@users.noreply.github.com>
2022-01-13 11:06:09 +0000

    turned seperate functions into class

--- Storage.py
+++ Storage.py
@@ -1,32 +1,37 @@
 import os
 file_path = 'Storage.txt'
 
-dataing = [["Username","ConvinientUsername"], ["Password","Fo0tBa1l12£"], ["Score",0]]
+dataing = [["Username","ConvenientUsername"], ["Password","Fo0tBa1l12£"], ["Score",0]]
 dataing2 = [["Username","LovePizza"], ["Password","RuGbY0!23"], ["Score",0]]
 
-def rData():
-    with open("Storage.txt", "r+") as f:
-        readData = f.read() 
-        print(readData)  
-        return readData  
+class storing:
+    def __init__(self):
+        self.dataing = dataing
+        self.dataing2 = dataing2
 
-def WorA(dataing): #Write or Append
-    if os.stat(file_path).st_size == 0:
-        emptyData = True
-    readData = rData()
-    if emptyData:    
-        with open('Storage.txt', 'w+') as f:
-            writeData = dataing
-            f.write(writeData)
-            return writeData
-    else:
-        with open('Storage.txt', 'a') as f:
-            appendData = dataing
-            f.write(appendData)
-            return appendData
+    def rData(self):
+        with open("Storage.txt", "r+") as f:
+            readData = f.read() 
+            print(readData)  
+            return readData  
+
+    def WorA(dataing): #Write or Append
+        if os.stat(file_path).st_size == 0:
+            emptyData = True
+        readData = storing.rData()
+        if emptyData:    
+            with open('Storage.txt', 'w+') as f:
+                writeData = dataing
+                f.write(writeData)
+                return writeData
+        else:
+            with open('Storage.txt', 'a') as f:
+                appendData = dataing
+                f.write(appendData)
+                return appendData
 
 choice = ""
-player = WorA(dataing)
+player = storing.WorA(dataing)
 #player2 = WorA(dataing2)
 print(player)
 #print(player2)
